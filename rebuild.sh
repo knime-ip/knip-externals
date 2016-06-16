8ce3e6cbcd0a2b5b3aa3adfc5ef8bc7bf2be541d
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2016-06-16 12:53:30 +0200

    add total build duration output to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SECONDS=0
+
 rm -rf target
 cd autogen
 mvn clean install
@@ -6,3 +8,6 @@ cd ../target/
 mvn clean install
 cd update-site
 mvn clean install
+
+duration=$SECONDS
+echo "Total buildtime: $(($duration / 60)) minutes and $(($duration %60)) seconds."

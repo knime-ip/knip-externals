955e2871e2ba9b2d9cf54d58f7476b1e9ab196ea
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2018-07-04 10:31:35 +0200

    rebuild.sh:  remove multi-threading argument from unsupported maven jobs

--- rebuild.sh
+++ rebuild.sh
@@ -3,11 +3,11 @@ SECONDS=0
 
 rm -rf target
 cd autogen
-mvn -T 1C clean install
+mvn clean install
 cd ../target/
 mvn -T 1C clean install
 cd update-site
-mvn -T 1C clean install
+mvn clean install
 
 duration=$SECONDS
 echo "Total buildtime: $(($duration / 60)) minutes and $(($duration %60)) seconds."
